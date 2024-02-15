d1608a9f99e7ff8ecb51cd3b7f67dc0418197836
spencer cap <spencercap@users.noreply.github.com>
2024-02-15 03:58:43 +0000

    Initial Commit

---
+++ spatial-pods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spatial-pods
+//
+//  Created by spencer cap on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spatial-pods/spatial_podsApp.swift
@@ -0,0 +1,17 @@
+//
+//  spatial_podsApp.swift
+//  spatial-pods
+//
+//  Created by spencer cap on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct spatial_podsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
